a846dd616d8f06cbd0194bc2ef5b1640c2fce71c
tadhgcreedon <creedon@cs.dal.ca>
2016-05-10 14:10:35 -0400

    added a little bit of style to page header

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return(
-      <div><h1>Blogging App</h1></div>
+      <div>
+        <h1>Blogging App</h1>
+        <span className="authorText">by Tadhg Creedon</span>
+        <br /><br />
+        <hr />
+      </div>
     );
   }
 }
