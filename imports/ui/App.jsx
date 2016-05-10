0f92dfb214b38f2c40ebfd5c959edfb9493f139c
tadhgcreedon <creedon@cs.dal.ca>
2016-05-10 14:13:14 -0400

    Moved Header component into separate file

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 
+// Import UI components
+import Header from './Header.jsx'
+
 export default class App extends Component {
   render() {
     return(
-      <div>
-        <h1>Blogging App</h1>
-        <span className="authorText">by Tadhg Creedon</span>
-        <br /><br />
-        <hr />
-      </div>
+      <Header />
     );
   }
 }
---
+++ imports/ui/Header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Blogging App</h1>
+        <span className="authorText">by Tadhg Creedon</span>
+        <br /><br />
+        <hr />
+      </div>
+    );
+  }
+}
