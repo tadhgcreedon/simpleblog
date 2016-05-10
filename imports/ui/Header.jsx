8d2e419f4dfb584a0a9222e2db0208ce01a63eba
tadhgcreedon <creedon@cs.dal.ca>
2016-05-10 16:35:11 -0400

    Added login capabilities, styled login a bit

--- imports/ui/Header.jsx
+++ imports/ui/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import AccountsUIWrapper from './AccountsUIWrapper.jsx';
 
 export default class Header extends Component {
   render() {
@@ -6,7 +7,18 @@ export default class Header extends Component {
       <header>
         <h1><a href="#">Blogging App</a></h1>
         <span id="authorText">by Tadhg Creedon</span>
+        <Login />
       </header>
+    );
+  }
+}
+
+class Login extends Component {
+  render(){
+    return(
+      <span id="loginContainer">
+        <AccountsUIWrapper />
+      </span>
     );
   }
 }
