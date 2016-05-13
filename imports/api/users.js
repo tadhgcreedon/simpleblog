e3345f179729e53383185b743403429b54fa2080
tadhgcreedon <creedon@cs.dal.ca>
2016-05-13 16:22:26 -0400

    Made sure only _id and username get sent to client with users collection

--- imports/api/users.js
+++ imports/api/users.js
@@ -3,6 +3,6 @@ import { Mongo } from 'meteor/mongo';
 
 if(Meteor.isServer) {
   Meteor.publish('users', function usersPublication() {
-    return Meteor.users.find({});
+    return Meteor.users.find({}, {fields: {_id:1, username:1}});
   });
 }
